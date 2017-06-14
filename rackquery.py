39f18ddd3d19d6ec0d8c72b7fda3372621730394
daustinbooth <maxsail@gmail.com>
2017-06-13 21:50:25 -0700

    Create rackquery.py

--- rackquery.py
+++ rackquery.py
@@ -1,20 +1,26 @@
-#!/usr/bin/env python
-from __future__ import print_function
-from mysqlrackconnect import connection
-import sys
-
+def make_dict(rackdata):
+    #Return a dictionary of name:ipaddr pairs.
+    
+    d={}
+    for name, ipaddr in rackdata:
+        d.setdefault(name, ipaddr)
+    return d
 
 def main(query):
-    
-    output = c.fetchall()
-    
+
+    #Open connection to racktables db and ask it for a full dump of hostname:ip associations 
     c, conn = connection()
     c.execute("select name, INET_NTOA(ip) FROM IPv4Address")
     
-
-    for n, i in output.items(): 
-            if query in n:          
-                print('Full match: "{}" is at: {}'.format(n, i))
+    #Send the list of tuples recieved to be converted into a dictionary.
+    rackdict = make_dict(rackdata = c.fetchall())
+    
+    #Loop through dictionary to find match.
+    for n, i in rackdict.items(): 
+        if query in n:
+            print ('The host "{}" has an IP of {} in Racktables.').format (n, i)
+        if query in i:
+            print ('The IP {} is associated with "{}" in Racktables.').format(i, n) 
 
     c.close()
     conn.close()
